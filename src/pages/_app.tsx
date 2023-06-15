9b652caa28d4f9bc17e673a403c303a6888e2d19
Coreytaz <coreytaz321@gmail.com>
2023-06-15 18:12:18 +0500

    change to page router

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import '@/styles/globals.scss'
+import { Header } from '@/widgets/Header'
+import { ChakraProvider } from '@chakra-ui/react'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Header />
+      <Component {...pageProps} />
+    </ChakraProvider>)
+}
